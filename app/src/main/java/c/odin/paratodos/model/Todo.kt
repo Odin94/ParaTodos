f7d5a4f093b938a69ff06c9d709a666ab6ff988a
Odin <kammerloher.andreas@gmail.com>
2019-10-06 18:53:21 +0200

    Add model for Todos

--- app/src/main/java/c/odin/paratodos/model/Todo.kt
+++ app/src/main/java/c/odin/paratodos/model/Todo.kt
@@ -1,3 +1,12 @@
 package c.odin.paratodos.model
 
 
+data class Todo(
+    val id: Int,
+    val date_created: String,
+    var title: String,
+    var description: String,
+    var priority: String,
+    var date_reminder: String,
+    var date_due: String
+)
